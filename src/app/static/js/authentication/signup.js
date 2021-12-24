593babf70b71331d675a2899440e67e9ec4db15f
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2021-12-24 12:30:59 +0400

    Signup JS base

--- src/app/static/js/authentication/signup.js
+++ src/app/static/js/authentication/signup.js
@@ -3,4 +3,42 @@ function signup(event){
 
     // Preventing reloading.
     event.preventDefault();
+
+    // Getting all fields.
+    let signupUsername = document.getElementById("signup-username");
+    let signupMail = document.getElementById("signup-mail");
+    let signupPassword = document.getElementById("signup-password");
+    let signupPasswordVerfiy = document.getElementById("signup-password-verify");
+
+    $.ajax({
+        "type": "POST",
+        "url": "/authentication/signup",
+        success: function (data){
+            if ("authentication" in data){
+                let authentication = data.authentication;
+
+                // Get authentication details.
+                let status = authentication.status;
+                let message = authentication.message;
+
+                if (status){
+                    // If success authentication.
+
+                    // Go root page.
+                    window.location.href = "/";
+                }else{
+                    // If any error.
+
+                    // Console.
+                    console.log("Got authentication API error! Message from the server: " + message);
+                }
+            }
+        },
+        error: function(xhr){
+            // If any error.
+
+            // Console.
+            console.log("Got authentication AJAX error! Message from the server: " + xhr.statusText);
+        }
+    })
 }
