661f6a30e5e200b8ac8f9b1290c4622b09bd28e0
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2021-12-27 22:00:29 +0400

    Refactor JS to TS

--- src/app/static/js/games/dice.ts
+++ src/app/static/js/games/dice.ts
@@ -1,15 +1,15 @@
-function diceBetPercentUpdateInput(value) {
+function diceBetPercentUpdateInput() {
     // Updates dice bet percent inpup text.
 
     // Update.
-    let betPercent = document.getElementById("dice-game-bet-percent-input").value;
-    if (betPercent != ""){
+    let betPercent: any = $("#dice-game-bet-percent-input").val();
+    if (betPercent != "" && typeof betPercent == "number"){
         if (betPercent < 1) betPercent = 1;
         if (betPercent > 95) betPercent = 95;
     }
 
     // Set value.
-    document.getElementById("dice-game-bet-percent-input").value = betPercent;
+    $("#dice-game-bet-percent-input").val(betPercent);
 
     // Re-update win size.
     diceBetPotentialWinSizeUpdate();
@@ -18,18 +18,18 @@ function diceBetPercentUpdateInput(value) {
     diceBetRangesUpdate();
 }
 
-function diceBetSizeUpdateInput(value){
+function diceBetSizeUpdateInput(){
     // Updates bet size input.
 
     // Update.
-    let betSize = document.getElementById("dice-game-bet-size-input").value;
-    if (betSize != ""){
+    let betSize: any = $("#dice-game-bet-size-input").val();
+    if (betSize != "" && typeof betSize == "number"){
         if (betSize < 1) betSize = 1;
         if (betSize > Number.MAX_SAFE_INTEGER) betSize = Number.MAX_SAFE_INTEGER;
     }
 
     // Set value.
-    document.getElementById("dice-game-bet-size-input").value = betSize;
+    $("#dice-game-bet-size-input").val(betSize);
 
     // Re-update win size.
     diceBetPotentialWinSizeUpdate();
@@ -42,43 +42,45 @@ function diceBetPotentialWinSizeUpdate(){
     // Updates potential win size information preview.
 
     // Get values.
-    let betPercent = parseInt(document.getElementById("dice-game-bet-percent-input").value);
-    let betSize = parseInt(document.getElementById("dice-game-bet-size-input").value);
+    let betPercent: number = Number($("#dice-game-bet-percent-input").val());
+    let betSize: number = Number($("#dice-game-bet-size-input").val());
 
     // Calculate win size.
-    let winSize = (100 / betPercent * betSize);
-    winSize = winSize.toFixed(2);
-
+    let winSize: number = -1;
+    if (typeof betSize == "number" && typeof betPercent == "number"){
+        winSize = (100 / betPercent * betSize);
+    }
+    
     // Set.
-    document.getElementById("dice-game-bet-potential-win-size").innerText = winSize;
+    $("#dice-game-bet-potential-win-size").text(winSize.toFixed(2));
 }
 
 function diceBetRangesUpdate(){
     // Updates bet ranges.
 
     // Get values.
-    let betPercent = parseInt(document.getElementById("dice-game-bet-percent-input").value);
+    let betPercent: any = $("#dice-game-bet-percent-input").val();
 
     // Max range for the bet value.
-    let betRange = 1000000;
+    let betRange: number = 1000000;
 
     // Difference for threshold max and min calculation.
-    let betRequiredThresholdDifference = (betRange / 100) * betPercent
+    let betRequiredThresholdDifference: number = (betRange / 100) * betPercent
 
     // Set.
-    document.getElementById("dice-game-bet-range-min").innerText = "0 - " + betRequiredThresholdDifference.toString();
-    document.getElementById("dice-game-bet-range-max").innerText = (betRange - betRequiredThresholdDifference).toString() + " - " + betRange.toString();
+    $("#dice-game-bet-range-min").text("0 - " + betRequiredThresholdDifference.toString());
+    $("#dice-game-bet-range-max").text((betRange - betRequiredThresholdDifference).toString() + " - " + betRange.toString());
 }
 
-function diceBetPlace(type){
+function diceBetPlace(type: string){
     // Places bet, by sending request and showing result.
 
     // Get values.
-    let betPercent = parseInt(document.getElementById("dice-game-bet-percent-input").value);
-    let betSize = parseInt(document.getElementById("dice-game-bet-size-input").value);
+    let betPercent: any = $("#dice-game-bet-percent-input").val();
+    let betSize: any = $("#dice-game-bet-size-input").val();
 
     // Create url.
-    let url = "/games/dice/?bet_size=" + betSize.toString() + "&bet_percent=" + betPercent.toString() + "&&bet_type=" + type.toString();
+    let url: string = "/games/dice/?bet_size=" + betSize.toString() + "&bet_percent=" + betPercent.toString() + "&&bet_type=" + type.toString();
 
     // POST.
     $.ajax({
@@ -86,15 +88,15 @@ function diceBetPlace(type){
       success: function(data){
         if ("bet_response" in data && "bet_result" in data["bet_response"]){
             let betResult = data["bet_response"]["bet_result"];
-            let betResults = document.getElementById("dice-game-bet-results");
+            let betResults = $("#dice-game-bet-results");
             if (betResult){
-                betResults.innerText = "Bet win!\n+ " + data["bet_response"]["bet_win_size"];
-                betResults.classList.remove("text-danger");
-                betResults.classList.add("text-success");
+                betResults.text("Bet win!\n+ " + data["bet_response"]["bet_win_size"]);
+                betResults.removeClass("text-danger");
+                betResults.addClass("text-success");
             }else{
-                betResults.innerText = "Bet lose!\n- " + data["bet_arguments"]["bet_size"];
-                betResults.classList.remove("text-success");
-                betResults.classList.add("text-danger");
+                betResults.text("Bet lose!\n- " + data["bet_arguments"]["bet_size"]);
+                betResults.removeClass("text-success");
+                betResults.addClass("text-danger");
             }
             return;
         }
@@ -103,16 +105,17 @@ function diceBetPlace(type){
       },
     });
 }
+
 window.addEventListener("load", function () {
     // Page is loaded.
 
     // Set default values.
     {
         // Bet percents.
-        let betPercent = 50;
-        document.getElementById("dice-game-bet-percent-input").value = betPercent;
-        diceBetPercentUpdateInput(betPercent);
+        $("#dice-game-bet-percent-input").val(50);
+        diceBetPercentUpdateInput();
     }
+
     // Potential win size.
     diceBetPotentialWinSizeUpdate();
 
