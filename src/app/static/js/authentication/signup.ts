341e407e8bdd6cab5d36b27682aab5f5afe67811
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2021-12-27 21:08:16 +0400

    NPM, TypeScript. (Used TS only in signup partially for now only

--- src/app/static/js/authentication/signup.ts
+++ src/app/static/js/authentication/signup.ts
@@ -1,8 +1,9 @@
-"use strict";
-function signup(event) {
+function signup(event: any){
     // Do signup submit.
+
     // Preventing reloading.
     event.preventDefault();
+
     // Send data.
     $.ajax({
         type: "POST",
@@ -14,53 +15,67 @@ function signup(event) {
             "password": $("#signup-password").val(),
             "password_confirmation": $("#signup-password-confirmation").val()
         },
-        success: function (data) {
-            if (!("authentication" in data)) {
+        success: function (data){
+            if (!("authentication" in data)){
                 // Error if invalid data.
                 signupError(data, "data-error");
                 return;
             }
-            if (!data.authentication.status) {
+
+            if (!data.authentication.status){
                 // If error when authenticating.
+
                 // Error.
                 signupError(data.authentication.message, "api-error");
                 return;
             }
+
             // Success.
             signupSuccess();
         },
-        error: function (xhr) {
+        error: function(xhr){
             // Error.
             let json = JSON.parse(xhr.responseText);
-            if ("error" in json) {
+            if ("error" in json){
                 signupError(json["error"], "api-error");
                 return;
             }
             signupError(xhr.statusText, "xhr-error");
         }
-    });
+    })
 }
-function signupError(text, name) {
+
+function signupError(text: string, name: string){
     // Error signup.
+
     // Message
-    signupMessage("<big class='text-danger'>Sorry, unexpected error occured!</big><br><b>"
+    signupMessage(
+        "<big class='text-danger'>Sorry, unexpected error occured!</big><br><b>"
         + text +
         "</b><br><small class='text-muted'>"
         + name +
-        "</small>");
+        "</small>"
+    );
 }
-function signupSuccess() {
+
+function signupSuccess(){
     // Successfully signup.
+
     // Messsage.
-    signupMessage("<big class='text-success'>Successfully authenticated!</big><br><small class='text-muted'>you will be redirected...</small>");
+    signupMessage(
+        "<big class='text-success'>Successfully authenticated!</big><br><small class='text-muted'>you will be redirected...</small>"
+    );
+
     // Go to root.
     window.location.href = "/";
 }
-function signupMessage(html) {
+
+function signupMessage(html: string){
     // Show message.
+
     // Message.
     let message = $("#ajax-message");
     message.html(html);
-    message.addClass("border");
+    message.addClass("border")
     message.show();
 }
