aa42b4f63621eaadef962873583827537c2b36c4
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2021-12-27 15:17:21 +0400

    A bit rework signup JS, AJAX messages improvements

--- src/app/static/js/authentication/signup.js
+++ src/app/static/js/authentication/signup.js
@@ -4,47 +4,61 @@ function signup(event){
     // Preventing reloading.
     event.preventDefault();
 
-    // Getting all fields.
-    let signupUsername = document.getElementById("signup-username").value;
-    let signupMail = document.getElementById("signup-mail").value;
-    let signupPassword = document.getElementById("signup-password").value;
-    let signupPasswordVerfiy = document.getElementById("signup-password-verify").value;
-
     $.ajax({
         type: "POST",
         url: "/authentication/signup",
         data: {
-            "username": signupUsername,
-            "mail": signupMail,
-            "password": signupPassword,
-            "passwordVerify": signupPasswordVerfiy
+            "username": $("#signup-username").value,
+            "mail": $("#signup-mail").value,
+            "password": $("#signup-password").value,
+            "passwordVerify": $("#signup-password-verify").value
         },
         success: function (data){
-            if ("authentication" in data){
-                // If this is valid data.
-
-                // Get authentication details.
-                let status = data.authentication.status;
-                let message = data.authentication.message;
-
-                if (status){
-                    // If success authentication.
-
-                    // Go root page.
-                    window.location.href = "/";
-                }else{
-                    // If any error.
-
-                    // Console.
-                    console.log("Got authentication API error! Message from the server: " + message);
-                }
+            if (!("authentication" in data)){
+                // Error if invalid data.
+                signupError(data, "data-error");
+                return;
             }
+
+            if (!data.authentication.status){
+                // If error when authenticating.
+
+                // Error.
+                signupError(data.authentication.message, "api-error");
+                return;
+            }
+
+            // Success.
+            signupSuccess();
         },
         error: function(xhr){
-            // If any error.
-
-            // Console.
-            console.log("Got authentication AJAX error! Message from the server: " + xhr.statusText);
+            // Error.
+            signupError(statusText, "xhr-error");
         }
     })
+}
+
+function signupError(message, name){
+    // Error signup.
+
+    // Message.
+    $("#ajax-message").html(
+        "<big class='text-danger'>Sorry, unexpected error occured!</big><br><b>"
+        + message +
+        "</b><br><small class='text-muted'>"
+        + name +
+        "</small>"
+    ).show();
+}
+
+function signupSuccess(){
+    // Successfully signup.
+
+    // Messsage.
+    $("#ajax-message").html(
+        "<big class='text-success'>Successfully authenticated!</big><br><small class='text-muted'>you will be redirected...</small>"
+    ).show();
+
+    // Go to root.
+    window.location.href = "/";
 }
